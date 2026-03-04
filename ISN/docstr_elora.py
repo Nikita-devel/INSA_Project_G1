d7fca213cbfb085bf26736ea97807832bcff8f64
elora452 <elomer@orange.fr>
2026-03-04 09:58:33 +0100

    ma partie pas finie

--- ISN/docstr_elora.py
+++ ISN/docstr_elora.py
@@ -11,25 +11,25 @@ print(quitter(quit))
 quit2 = False
 print(quitter(quit2))
 
-def trouver_IE(calendrier):
+def trouver_IE(calendrier_trie): # je me suis trompee et j'ai fait la partie de Lina
     """ Renvoie la date de la prochaine IE à partir de la date d'aujourd'hui
     Parameters:
-    - calendrier: cal. Calendrier de l'utilisateur
+    - calendrier: cal. Calendrier trie de l'utilisateur
     Return
     - date_IE: date. Date de la plus proche IE. Renvoie None si c'est la fin de l'année. 
    """
-    date debut = aujourdhui()
-    date fin = #dernier evenement du calendrier
-    evenements = liste_evenement(calendrier, date debut, date fin)
+    date_debut = aujourdhui()
+    date_fin = extraire_date_event(calendrier_trie[-1]) #derniere heure de cours du calendrier
+    evenements = liste_evenement(calendrier_trie, date debut, date fin) #mettre tous les evenements en liste
     i = 0
     IE = False
-    date_IE = 0
     while i < len(evenements) and not IE :
-        if "IE" in evenements[i] :
+        if "EV" in evenements[i] :
             IE = True
-            date_IE = extraire_date_event(evenement[i])
+            date_IE = extraire_date_event(evenements[i])
+            matiere_IE = evenements[i][5] #trouver matiere en prenant 6eme colonne excel
+            print(f"Nous sommes le {aujourdhui}.")
+            print(f"La prochaine évaluation sera : {matiere_IE} le {date_IE[2]} {jour_semaine(evenements[i]} de {date_IE[3]}h à {extraire_date_event(evenements[i], False}h.")
         i += 1
-        
-    
-    
-
+    if not IE :
+        print("PLUS D'IE AHAHAHAH")
